5c776fffd5a7c2582a24a2d433e2db9729ddb64f
Abhishek Yadav <monuy8830@gmail.com>
2025-07-01 16:38:18 +0530

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- data/PlywoodData/categoryData.js
+++ data/PlywoodData/categoryData.js
@@ -1,43 +1,42 @@
 export const categories = [
-    {
-        categoryId: 1,
-        title: "Plywood",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 2,
-        title: "Laminate",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 3,
-        title: "Veneer",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 4,
-        title: "Doors",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 5,
-        title: "Table",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 6,
-        title: "Chair",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 7,
-        title: "Block Board",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-    {
-        categoryId: 8,
-        title: "Cane webbing",
-        image: "https://picsum.photos/200/300?random=1",
-    },
-
-]
+  {
+    categoryId: 1,
+    title: "Plywood",
+    image: "/img/plywoodWale/PremiumPlywood/1.jpeg",
+  },
+  {
+    categoryId: 2,
+    title: "Laminate",
+    image: "/img/plywoodWale/placeholderImages/36.jpeg",
+  },
+  {
+    categoryId: 3,
+    title: "Veneer",
+    image: "/img/plywoodWale/placeholderImages/17.jpeg",
+  },
+  {
+    categoryId: 4,
+    title: "Doors",
+    image: "/img/plywoodWale/placeholderImages/20.jpeg",
+  },
+  {
+    categoryId: 5,
+    title: "Table",
+    image: "/img/plywoodWale/placeholderImages/9.jpeg",
+  },
+  {
+    categoryId: 6,
+    title: "Chair",
+    image: "/img/plywoodWale/placeholderImages/38.jpeg",
+  },
+  {
+    categoryId: 7,
+    title: "Block Board",
+    image: "/img/plywoodWale/placeholderImages/39.jpeg",
+  },
+  {
+    categoryId: 8,
+    title: "Cane webbing",
+    image: "/img/plywoodWale/placeholderImages/40.jpeg",
+  },
+];
